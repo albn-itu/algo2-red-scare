22d35e9fc17cb7aa0f1a534512629e4b8b600019
Albert Rise Nielsen <albert@risenielsen.dk>
2023-10-25 15:30:57 +0200

    Some scaffolding

--- code/main.py
+++ code/main.py
@@ -1,2 +1,28 @@
-import graph
 import parsing
+import algorithms
+
+
+def none():
+    pass
+
+
+def some():
+    pass
+
+
+def few():
+    pass
+
+
+def many():
+    pass
+
+
+def alternate():
+    pass
+
+
+if __name__ == '__main__':
+    parsing.open_and_parse('../data/G-ex.txt')
+
+    # TODO: Run the methods here:
