fa43338f64b805f9ad79cbc7ceb823a41e04cc7f
Albert Rise Nielsen <albert@risenielsen.dk>
2023-10-26 08:33:02 +0200

    Parse file based on argument to main

--- code/main.py
+++ code/main.py
@@ -1,4 +1,5 @@
 import parsing
+from argparse import ArgumentParser
 import algorithms
 
 
@@ -23,5 +24,13 @@ def alternate():
 
 
 if __name__ == '__main__':
+    parser = ArgumentParser(description='Red scare')
+    parser.add_argument('-f', '--file', default='../data/G-ex.txt',
+                        required=False, help='The file to be parsed')
+    args = parser.parse_args()
+
+    # TODO: Hail mary argument that just runs all the files
     # TODO: Parsing
     # TODO: Run the methods here:
+    graph = parsing.open_and_parse(args.file)
+    print(graph)
