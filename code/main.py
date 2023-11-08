b480bf3907e26ae61a2a1bb2c1462c0954396eda
Albert Rise Nielsen <albert@risenielsen.dk>
2023-11-08 11:41:20 +0100

    Get and write results

--- code/main.py
+++ code/main.py
@@ -1,8 +1,9 @@
+import os
 import parsing
 from argparse import ArgumentParser
 from algorithms import alternating_bfs, ignoring_red_vertices_bfs
 from copy import deepcopy
-from utils import get_path_length, print_dict
+from utils import get_path_length, val_or_na, write_results
 
 
 def none(graph):
@@ -10,15 +11,15 @@ def none(graph):
     return get_path_length(graph, parent)
 
 
-def some():
+def some(graph):
     pass
 
 
-def few():
+def few(graph):
     pass
 
 
-def many():
+def many(graph):
     pass
 
 
@@ -31,15 +32,51 @@ def alternate(graph):
         return 'false'
 
 
+def run(graph):
+    a = val_or_na(alternate(deepcopy(graph)))
+    f = val_or_na(few(deepcopy(graph)))
+    m = val_or_na(many(deepcopy(graph)))
+    n = val_or_na(none(deepcopy(graph)))
+    s = val_or_na(some(deepcopy(graph)))
+
+    return [graph.name, graph.n, a, f, m, n, s]
+
+
+def gather_results():
+    files = os.listdir('../data')
+    ignored = set(["README.md"])
+    results = []
+
+    for i, file in enumerate(files):
+        print(f"Running {i+1}/{len(files)} ({file})")
+
+        if file in ignored:
+            continue
+
+        graph = parsing.open_and_parse('../data/' + file)
+        results.append(run(graph))
+
+    return results
+
+
 if __name__ == '__main__':
     parser = ArgumentParser(description='Red scare')
     parser.add_argument('-f', '--file', default='../data/G-ex.txt',
                         required=False, help='The file to be parsed')
+    parser.add_argument('-a', '--all', action='store_true',)
     args = parser.parse_args()
 
-    # TODO: Hail mary argument that just runs all the files
-    # TODO: Run the methods here:
-    graph = parsing.open_and_parse(args.file)
+    if args.all:
+        results = gather_results()
+        write_results(results)
+    else:
+        graph = parsing.open_and_parse(args.file)
+        res = run(graph)
 
-    print("alternate", alternate(deepcopy(graph)))
-    print("none", none(deepcopy(graph)))
+        print(f"name:\t{res[0]}")
+        print(f"n:\t{res[1]}")
+        print(f"Alt:\t{res[2]}")
+        print(f"Few:\t{res[3]}")
+        print(f"Man:\t{res[4]}")
+        print(f"Non:\t{res[5]}")
+        print(f"Som:\t{res[6]}")
