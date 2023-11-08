48b9cd83994ba45321c945b8d0f8a25fd92aed46
Andreas Tietgen <9461145+Arneproductions@users.noreply.github.com>
2023-11-08 08:26:42 +0100

    Add inital topological sort

--- code/algorithms.py
+++ code/algorithms.py
@@ -1,4 +1,5 @@
 # TODO: Write your algorithm code here. Such as BFS, DFS, A*, Ford-Fulkerson, etc.
+from graph import Graph, Node
 
 def BFS_ignoring_red_vertices(graph) :
     queue = [] 
@@ -18,3 +19,60 @@ def BFS_ignoring_red_vertices(graph) :
                     parent[e] = v
                     queue.append(e)
     return parent
+
+def topological_sort(graph: Graph):
+    """
+    Sorts the graph in a topological order
+
+    Returns: a list with the nodes in topological order
+    """
+
+    def dfs(graph: Graph, visited, sorted_nodes):
+        """
+        Performs depth first search on a graph 
+        """
+
+        stack = []
+
+        stack.append((graph.start, False))
+
+        while len(stack):
+            current, is_back = stack.pop()
+
+            if is_back:
+                sorted_nodes.append(current)
+                continue
+
+            visited.add(current)
+            stack.append((current, True)) # Add trace back
+
+            for neighbor in graph.nodes[current]:
+                if neighbor not in visited:
+                    stack.append((neighbor, False))
+
+    visited = set()
+    sorted_nodes = []
+
+    for node in graph.nodes:
+        if node not in visited:
+            dfs(graph, node, visited, sorted_nodes)
+
+    return sorted_nodes
+
+def longest_chain(n, nodes, sorted_nodes):
+    dist = [-1] * (n + 1)
+    dist[sorted_nodes[-1]] = 0
+    max_dist = -1
+
+    while sorted_nodes:
+        node = sorted_nodes.pop()
+
+        if dist[node] != -1:
+            for neighbor in nodes[node]:
+                if dist[neighbor] < dist[node] + 1:
+                    dist[neighbor] = dist[node] + 1
+
+                    if dist[neighbor] > max_dist:
+                        max_dist = dist[neighbor]
+
+    return max_dist
